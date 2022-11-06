a065a3a8d55262b7a37262b0fb4b00180448a8b1
Rafael Ricciardi <ricciardi.rafael1997@gmail.com>
2022-11-06 02:18:35 -0300

    db structure, create pool route, and routes to counting guesses and users

---
+++ server/prisma/seed.ts
@@ -0,0 +1,60 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const user = await prisma.user.create({
+    data: {
+      name: "John Doe",
+      email: "john.doe@gmail.com",
+      avatarURL: "https://github.com/ricciardi305.png",
+    },
+  });
+
+  const pool = await prisma.pool.create({
+    data: {
+      title: "Pool Example",
+      code: "BOL123",
+      ownerId: user.id,
+
+      participants: {
+        create: {
+          userId: user.id,
+        },
+      },
+    },
+  });
+
+  await prisma.game.create({
+    data: {
+      date: "2022-11-07T09:00:00.000Z",
+      firstTeamCountryCode: "DE",
+      secondTeamCountryCode: "BR",
+    },
+  });
+
+  await prisma.game.create({
+    data: {
+      date: "2022-11-07T12:00:00.000Z",
+      firstTeamCountryCode: "CH",
+      secondTeamCountryCode: "BR",
+      guesses: {
+        create: {
+          firstTeamPoints: 1,
+          secondTeamPoints: 2,
+
+          participant: {
+            connect: {
+              userId_poolId: {
+                userId: user.id,
+                poolId: pool.id,
+              },
+            },
+          },
+        },
+      },
+    },
+  });
+}
+
+main();
